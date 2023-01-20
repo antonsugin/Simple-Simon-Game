2c0c37180a0ccd209d4d7985af2e46075138f11c
Emma Paris <suginant@gmail.com>
2023-01-19 21:14:05 -0500

    Modified css, index.html and index.js

--- index.js
+++ index.js
@@ -1,37 +1,78 @@
-document.getElementById("start").onclick = () => {random()};
+window.onload = () => {
+    colorRender();
+
+}
+
+
+
+document.getElementById("start").onclick = () => {isEqual(choosenColors,activeColors )};
 
 let start = () => {
     document.getElementById("blue").classList.add("is-active");
 }
 
-const coloris = {
-    // "1" : { "color" : "blue", "id" : 1 },
-    // green : { "color" : "green", "id" : 2 },
-    // red : { "color" : "red", "id" : 3 },
-    // yellow : { "color" : "yellow", "id" : 4 },
-    'blue' : 1,
-    'green' : 2,
-    'red' : 3,
-    'yellow' : 4
-}
-
-// blue=1;
-// green=2;
-// red=3;
-// yellow=4;
 
 
 const colors = ['blue','green','red','yellow'];
-
-let random = () => {
-    let num = Math.floor(Math.random() * colors.length);
-
-    document.getElementById(colors[num]).classList.add("is-active");
+let activeColors = [];
+let choosenColors = [];
 
 
-    console.log(colors[num]);
-    console.log(num);
+
+let colorRender = () => {
+    let ind = Math.floor(Math.random() * colors.length);
+
+    document.getElementById(colors[ind]).classList.add("is-active");
+    activeColors.push(colors[ind]);
+    setTimeout(() => {document.getElementById(colors[ind]).classList.remove("is-active");}, 200);
+    console.log(activeColors)
+    // isTrue(activeColorInd)
+
 }
+
+let isEqual = (activeColorsArray, choosenColorsArray) => {
+    // console.log (array[0] === choosenColor[0] ? true : false);
+    console.log(JSON.stringify(activeColorsArray) === JSON.stringify(choosenColorsArray))
+    // console.log (choosenColor)
+    if (activeColorsArray.length === choosenColorsArray.length) {
+        if (JSON.stringify(activeColorsArray) === JSON.stringify(choosenColorsArray)){
+            colorRender();
+            choosenColors = [];
+        }
+        else {
+            alert('false')
+        }
+
+    }
+    // else {
+
+    // }
+}
+
+// isTrue (activeColorInd,choosenColor )
+// document.addEventListener("click", mySecondFunction);
+
+// const buttonPressed = e => { 
+//     result.innerHTML = `ID of <em>${e.target.innerHTML}</em> is <strong>${e.target.id}</strong>`;
+//   }
+  
+//   for (let button of buttons) {
+//     button.addEventListener("click", buttonPressed);
+//   }
+  
+
+const gameColors = document.getElementsByClassName('color');
+
+const colorPressed = event => {
+    choosenColors.push(event.target.id);
+    isEqual(choosenColors,activeColors )
+}
+
+for (let color of gameColors) {
+    color.addEventListener("click", colorPressed);
+  }
+
+// colorPressed()
 
 let chooseColor = (color) => {
     
@@ -39,5 +80,3 @@ let chooseColor = (color) => {
 
 }
 
-// let b = coloris.'1'.color;
-// console.log(b)
