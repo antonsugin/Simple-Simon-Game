4c033438fa3a2770226f8e5b46c7c51f041014d1
Emma Paris <suginant@gmail.com>
2023-01-23 21:29:56 -0500

    Modified index.js and index.html, style.css, added sounds folder

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 window.onload = () => {
     setNames ();
-    
-
-    // setTimeout(function () {
-    //     document.getElementById("header").innerHTML = 'Ready Player ' + players.namePlayerOne
-        
-    //     }, 1000);
-
-    setTimeout(function () {
-        colorRender();
-        // document.getElementById("header").innerHTML = 'Level ' + level;
-        
-        }, 500);
+    header(players.namePlayerOne);
 
 }
 
-
-
+const audio = new Audio ('sounds/wrong.mp3');
+const colors = ['blue','green','red','yellow'];
+const gameColors = document.getElementsByClassName('color');
+let level = 0;
+let personPlayer = '';
+let activeColors = [];
+let choosenColors = [];
 let players = {
     "namePlayerOne" : "Player 1",
     "levelPlayerOne" : 0,
     "namePlayerTwo" : "Player 2",
     "levelPlayerTwo" : 0
+};
+
+
+
+const header = (player) => {
+    level = 0;
+
+    setTimeout(function () {
+        document.getElementById("header").innerHTML = 'Ready Player ' + player;
+        
+    }, 500);
+
+    setTimeout(function () {
+        colorRender();
+        document.getElementById("header").innerHTML = 'Level ' + level;
+        
+    }, 2000);
+    
 }
 
-
-// console.log(players.namePlayerOne)
-let level = 0;
-// let playerName = "Player 1";
-// let personName = "Player 2";
 
 
 const starter = () => {
@@ -36,14 +43,9 @@ const starter = () => {
 
 }
 
-let personPlayer = '';
+
 
 let setNames = () => {
-
-    // enabled();
-    // restart();
-    
-    // document.getElementById("header").innerHTML = "Good Luck!!!";
 
     let capitalizeFirstLetter = (name) => name.charAt(0).toUpperCase() + name.slice(1);
 
@@ -51,10 +53,8 @@ let setNames = () => {
     let person = prompt("Please enter your name", "Player 2");
 
     if (players.namePlayerOne !== null && person !== null) {
-    document.getElementById("prompt-name1").innerHTML = capitalizeFirstLetter(player);
-    // document.getElementById("prompt-player1").innerHTML = capitalizeFirstLetter(player);
-    document.getElementById("prompt-name2").innerHTML = capitalizeFirstLetter(person);
-    // document.getElementById("prompt-player2").innerHTML = capitalizeFirstLetter(person);
+        document.getElementById("prompt-name1").innerHTML = capitalizeFirstLetter(player) +":";
+        document.getElementById("prompt-name2").innerHTML = capitalizeFirstLetter(person) + ":";
     }
 
     players.namePlayerOne = capitalizeFirstLetter(player);
@@ -63,81 +63,51 @@ let setNames = () => {
 }
 
 
-
-
-// document.getElementById("start").onclick = () => {isEqual(choosenColors,activeColors )};
-
-let start = () => {
-    document.getElementById("blue").classList.add("is-active");
-}
-
-
-
-const colors = ['blue','green','red','yellow'];
-let activeColors = [];
-let choosenColors = [];
-
-
-
 let colorRender = () => {
-    level++
-    document.getElementById("header").innerHTML = 'Level ' + level
+    level++;
+    document.getElementById("header").innerHTML = 'Level ' + level;
     let ind = Math.floor(Math.random() * colors.length);
 
     document.getElementById(colors[ind]).classList.add("is-active");
     activeColors.push(colors[ind]);
     choosenColors = [];
-    setTimeout(() => {document.getElementById(colors[ind]).classList.remove("is-active");}, 200);
-    console.log(activeColors)
-
-    
+    setTimeout(() => {document.getElementById(colors[ind]).classList.remove("is-active");}, 200);    
 }
-
-const checkLevel = (level) => {
-    // alert('hi')
-    console.log(personPlayer)
-    if (personPlayer) {
-        document.getElementById("win1").innerHTML = players.levelPlayerOne ='Level ' + (level - 1)
-    }
-    else players.levelPlayerTwo = level - 1
-}
-
-
-
-
-// console.log(level)
-// activeColorsArray, choosenColorsArray
-
-// const restartButton = document.getElementById("restart").addEventListener("click", colorRender, 
-// () => {
-//     activeColors = [];
-// });
 
 let isEqual = (currentLevel) => {
 
     if (activeColors[currentLevel] === choosenColors[currentLevel]) {
         if (choosenColors.length === activeColors.length){
-        //   setTimeout(function () {
-        //     colorRender();
-        //   }, 1000);
+        
         colorRender();
         }
       }
       else {
+        wrong();
         activeColors = [];
-        // console.log(level)
-        checkLevel(level)
+
+        checkLevel(level);
+        
+        
+        console.log(personPlayer)
+        if (personPlayer !== '') {
+            personPlayer = players.namePlayerTwo;
+            header(players.namePlayerTwo);
+        }
       }
 
 }    
 
-const gameColors = document.getElementsByClassName('color');
 
 const colorPressed = event => {
+    
     choosenColors.push(event.target.id);
 
-    console.log(activeColors)
-    console.log(choosenColors)
+    document.getElementById(event.target.id).classList.add("is-pressed");
+    setTimeout(function () {
+        document.getElementById(event.target.id).classList.remove("is-pressed");
+      }, 200);
+
     isEqual(choosenColors.length - 1)
 }
     
@@ -147,166 +117,65 @@ for (let color of gameColors) {
   }
 
 
-
-
-
-
-
-
-
-// let chooseColor = () => {
+const checkLevel = (level) => {
     
-//     for (let color of gameColors) {
-//         color.addEventListener("click", colorPressed);
-//       }
-
-// }
-
-
-// TESTS!!!!!!!!!!!!!!!!!!!!!
-
-// function Equal()
-//     {
-     
-//      var a = [1, 2, 3, 5];
-//      var b = [1, 2, 4, 5];
-      
-//       // If length is not equal
-//     //   if(a.length!=b.length)
-//     //    return "False";
-//     //   else
-//     //   {
-       
-//       // Comparing each element of array
-//        for(let i=0;i<a.length;i++)
-//        if(f[i]!=t[i]) {
-//         // console.log(f[i]);
-//         // console.log(t[i]);
-//        return "False";
-       
-
-//        }return "True";
-//     //   }
-//     }
-//     var v = Equal();
-//     // console.log(v);
-
-const compareArrays = (a, b) =>
- 
-  a.every((element, index) => element === b[index]);
-
-  let kar = [];
-  const f = ['blue','green','red','yellow'];
-  
-  const t = ['blue','green','yellow']; 
-
-  kar = f.slice(0,t.length)
-
-//   console.log(f.slice(0,t.length))
-//   console.log(kar === t)
-//   console.log(kar)
-//   console.log(t)
-//   f.copyWithin(2,0)
-//   console.log(f.slice(0,t.length) === kar)
-
-function Equal(a, b)
-    {
-    return a.join() == b.join();
-    }
-
-    // console.log(Equal(kar, t))
-
-
-
-
-
-    // let newChoosenColorsArray = [];
-    // const f = ['blue','green','red','yellow'];
-  
-    // const t = ['red','green','red'];   
-
-    //     console.log(activeColorsArray)
-    //     console.log(choosenColorsArray)
-
-    // if (activeColorsArray[0] === choosenColorsArray[0]) {
-    //     return true
-    // }
-    // else if (activeColorsArray[0] === choosenColorsArray[0] && activeColorsArray.length !== choosenColorsArray.length) {
-
-    
-        // let newChoosenColorsArray = activeColorsArray.slice(0,choosenColorsArray.length)
-
-        // console.log(newChoosenColorsArray)
-        // console.log(choosenColorsArray)
-
-        // return JSON.stringify(newChoosenColorsArray) === JSON.stringify(choosenColorsArray) ? true : false;
-    
-        // let newChoosenColorsArray = activeColorsArray.slice(0,choosenColorsArray.length)
-
-        // console.log(newChoosenColorsArray)
-        // console.log(choosenColorsArray)
-        // console.log(JSON.stringify(newChoosenColorsArray))
-        // console.log(activeColors)
-
-        // return JSON.stringify(newChoosenColorsArray) === JSON.stringify(choosenColorsArray) ? true : false;
-    // }
-    // else {
-    //     return false
-// }
-
-    
-
-//   console.log(f.splice(0,t.length))
-//   console.log(kar === t)
-//   console.log(kar)
-//   console.log(t)
-//   f.copyWithin(2,0)
-//   console.log(f.splice(0,t.length) === kar)
-
-// function Equal(a, b)
-//     {
-//     return a.join() == b.join();
-//     }
-
-//     console.log(Equal(kar, t))
-
-
-
-// }
-
-
-
-// if (activeColors[0] === choosenColors[0] && activeColors.length === choosenColors.length) {
-    //     colorRender();
-    // }
-    // else if (activeColors.length !== choosenColors.length) {
-    //     if (isEqual(activeColors, choosenColors) && activeColors.length === choosenColors.length) {
-    //         colorRender();
-    //         // chooseColor();
-    //     }
-    //     else {
-    //         alert('false 104')
-    //     }
-    // }
-    // // else if (isEqual(activeColors, choosenColors)) {
-    // //     colorRender();
-    // // }
-    // // else if (activeColors.length !== choosenColors.length) {
-    // //     // alert('ok')
-    // // }
-    // else if (activeColors[0] !== choosenColors[0]) {
-    //     alert('false 114') 
-    // }
-    // else {
+    if (personPlayer === players.namePlayerOne) {
+        players.levelPlayerOne = level - 1;
+        document.getElementById("win1").innerHTML = 'Level ' + players.levelPlayerOne;
         
-    //     alert('false 118')
-    // }
-    // // colorRender(); 
-    // console.log(choosenColors)
+    }
+    else {
+        players.levelPlayerTwo = level - 1;
+        document.getElementById("win2").innerHTML = 'Level ' + players.levelPlayerTwo;
+        console.log(checkWinner(players.levelPlayerOne,players.levelPlayerTwo));
+        personPlayer = '';
+        document.getElementById("restart").style.visibility = "visible";
+    }
+}
+
+const checkWinner = (levelOne, levelTwo) => {
+
+    if (levelOne === levelTwo ) document.getElementById("header").innerHTML = "Draw!!!";
+    else {
+
+        levelOne > levelTwo 
+        ? document.getElementById("header").innerHTML = players.namePlayerOne + " Wins The Game!!!"
+        : document.getElementById("header").innerHTML = players.namePlayerTwo + " Wins The Game!!!";
+    }
     
-// }
+ }
 
-// console.log(choosenColors)
-// console.log(activeColors)
 
-  
+document.getElementById("restart").onclick = () => {
+
+    document.getElementById("win1").innerHTML = 'Good Luck!';
+    document.getElementById("win2").innerHTML = 'Good Luck!';
+    personPlayer = players.namePlayerOne;
+    activeColors = [];
+    header(players.namePlayerOne);
+    document.getElementById("restart").style.visibility = "hidden";
+
+};
+
+document.getElementById("start").onclick = () => {
+    document.getElementById("win1").innerHTML = 'Good Luck!';
+    document.getElementById("win2").innerHTML = 'Good Luck!';
+    activeColors = [];
+    document.getElementById("restart").style.visibility = "hidden";
+
+    setNames ();
+    header(players.namePlayerOne);
+};
+
+const wrong = () => {
+    
+    audio.play();
+
+    document.getElementsByTagName('body')[0].classList.add('wrong');
+
+    setTimeout(function () {
+        document.getElementsByTagName('body')[0].classList.remove('wrong');
+        
+    }, 200);
+}
+
